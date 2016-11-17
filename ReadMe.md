07611ffebc033892f337b0a399992c67b9052b4c
姚正发 <976691141@qq.com>
2016-11-17 12:52:59 +0800

    添加测试与说明

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+#BearyChat机器人Incoming SDK
+为了便于以后运维监控的即时通知（非邮件），将采用BearyChat进行通知，故开发了此SDK
+
+## 使用说明
+直接通过实例化BearyChatClient对象并通过Send多种重载方式即可。
---
+++ test/UnitTest/Test.cs
@@ -0,0 +1,25 @@
+﻿using BearyChat.Incoming;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTest
+{
+    public class Test
+    {
+        private BearyChatClient _client;
+
+        public Test()
+        {
+            _client = new BearyChatClient("https://hook.bearychat.com/=bw9mn/incoming/d51dc1ae6e9da6371a1258af4bb70208");
+        }
+
+        [Fact]
+        public void Send()
+        {
+            _client.Send("Sino程序自动发送", "所有人");
+        }
+    }
+}
